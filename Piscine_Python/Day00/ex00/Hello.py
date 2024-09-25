0560cc8782d8e20451abbbecdb8c0bc4b6036f7a
plam <plam@student.42.fr>
2024-09-25 14:56:38 +0200

    change in ex00

--- Piscine_Python/Day00/ex00/Hello.py
+++ Piscine_Python/Day00/ex00/Hello.py
@@ -3,14 +3,17 @@ ft_tuple = ("Hello", "toto!")
 ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello" : "titi!"}
 
-
-#your code here
+# your code here
 
 ft_list[1] = "World!"
-ft_tuple[1] = "France!"
+ft_tuple_list = list(ft_tuple)
+ft_tuple_list[1] = "France!"
+ft_tuple = tuple(ft_tuple_list)
 ft_set.remove("tutu!")
 ft_set.add("Paris!")
 ft_dict["titi!"] = "42Paris!"
+
+#
 
 print(ft_list)
 print(ft_tuple)
