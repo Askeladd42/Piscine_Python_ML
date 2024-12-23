328f548d4b3ce461214e352106662da5f625ae59
Pierre-marie LAM <plam@paul-f4Br3s4.clusters.42paris.fr>
2024-12-23 15:59:52 +0100

    ex02 finished

--- Piscine_Python/Day00/ex02/find_ft_type.py
+++ Piscine_Python/Day00/ex02/find_ft_type.py
@@ -1,2 +1,20 @@
 def all_thing_is_obj(object: any) -> int:
 #your code here
+    content = ""
+    if type(object) == str:
+        content = object + " is in the kitchen"
+    elif type(object) == list:
+        content = "List"
+    elif type(object) == tuple:
+        content = "Tuple"
+    elif type(object) == set:
+        content = "Set"
+    elif type(object) == dict:
+        content = "Dict"
+    else:
+        content = "unknown"
+    if content == "unknown":
+        print("Type not found")
+    else:
+        print(f"{content} : {type(object)}")
+    return 42
