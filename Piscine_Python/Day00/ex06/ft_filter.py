a9b9b8f674ebd235073552bfa519554fe424d26d
plam <plam@student.42.fr>
2025-01-11 00:38:21 +0100

    ex06 started, took example on the python function filter & need more tests

--- Piscine_Python/Day00/ex06/ft_filter.py
+++ Piscine_Python/Day00/ex06/ft_filter.py
@@ -1,8 +1,20 @@
 import sys
 
+def ft_filter(function, iterable):
+    """
+    ft_filter(function or None, iterable) --> filter object
+
+    Return an iterator yielding those items of iterable for which function(item)
+    is true. If function is None, return the items that are true.
+    """
+    if function is None:
+        return (item for item in iterable if item)
+    else:
+        return (item for item in iterable if function(item))
+
 def main():
     # your tests and your error handling
-
+    pass
 
 if __name__ == "__main__":
     main()
