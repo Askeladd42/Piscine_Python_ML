bbc147c164bb6b6401761c3771ebd80bc895b3b3
plam <plam@student.42.fr>
2025-01-13 19:13:31 +0100

    ex08 started, need more tests & assertionError text correction

--- Piscine_Python/Day00/ex08/Loading.py
+++ Piscine_Python/Day00/ex08/Loading.py
@@ -1,8 +1,14 @@
-import sys
+import time
 
-def main():
-    # your tests and your error handling
-
-
-if __name__ == "__main__":
-    main()
+def ft_tqdm(lst: range):
+  # your code here
+    total = len(lst)
+    bar_length = 40
+    for i, item in enumerate(lst):
+        percent = (i + 1) / total * 100
+        filled_length = int(bar_length * (i + 1) // total)
+        bar = '=' * filled_length + '-' * (bar_length - filled_length)
+        print(f'\r|{bar}| {percent:.2f}%', end='\r')
+        yield item
+        time.sleep(0.1)  # Simulate work being done
+    print()
