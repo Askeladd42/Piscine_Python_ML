f5d1a7f4ab130d6fdb7d991219d3c89ba1102a32
Pierre-marie LAM <plam@bess-f2r6s1.clusters.42paris.fr>
2025-01-20 13:21:11 +0100

    day01 started, tests done for day00

--- Piscine_Python/Day01/ex00/give_bmi.py
+++ Piscine_Python/Day01/ex00/give_bmi.py
@@ -1,1 +1,8 @@
 import sys
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    # your code here
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    # your code here
