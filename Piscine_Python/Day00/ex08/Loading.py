10da4125426b1ef6ed6b58735521bcf718bba6d2
Pierre-marie LAM <plam@bess-f2r5s10.clusters.42paris.fr>
2025-01-22 12:42:05 +0100

    corrected the ex08 of day00 to conform to the instructions

--- Piscine_Python/Day00/ex08/Loading.py
+++ Piscine_Python/Day00/ex08/Loading.py
@@ -1,16 +1,14 @@
-import time
-
-
 def ft_tqdm(lst: range):
     """A simple tqdm implementation: prints a progress bar
-    and percentage of completion."""
+    and percentage of completion. Since no functions are allowed, not adapted
+    to all size of terminal compared to the original tqdm."""
     total = len(lst)
-    bar_length = 40
+    bar_length = 61  # size of the progress bar, to ajust manually if needed
     for i, item in enumerate(lst):
         percent = (i + 1) / total * 100
         filled_length = int(bar_length * (i + 1) // total)
-        bar = '=' * filled_length + '-' * (bar_length - filled_length)
-        print(f'\r|{bar}| {percent:.2f}%', end='\r')
+        bar = '[' + '=' * filled_length + '>' + ']' + \
+              '-' * (bar_length - filled_length)
+        print(f'\r{percent:3.0f}%|{bar}| {i + 1}/{total}', end='\r')
         yield item
-        time.sleep(0.1)  # Simulate work being done
     print()
