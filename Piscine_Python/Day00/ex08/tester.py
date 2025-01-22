9543b4cef93f6317240a4c7e1642ba429dc36958
Pierre-marie LAM <plam@bess-f2r5s10.clusters.42paris.fr>
2025-01-22 11:16:32 +0100

    ex01 started, added the tester

--- Piscine_Python/Day00/ex08/tester.py
+++ Piscine_Python/Day00/ex08/tester.py
@@ -2,10 +2,12 @@ from time import sleep
 from tqdm import tqdm
 from Loading import ft_tqdm
 
+print("Using ft_tqdm:")
 for elem in ft_tqdm(range(333)):
     sleep(0.005)
 print()
 
+print("Using tqdm:")
 for elem in tqdm(range(333)):
     sleep(0.005)
 print()
