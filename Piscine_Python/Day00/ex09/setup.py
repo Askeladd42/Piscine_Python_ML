6ef92e69a2812724b10f8309fdb333418d4da19b
Pierre-marie LAM <plam@bess-f2r5s10.clusters.42paris.fr>
2025-01-22 11:16:02 +0100

    modified the summary line to conform to the subject

--- Piscine_Python/Day00/ex09/setup.py
+++ Piscine_Python/Day00/ex09/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="ft_package",
     version="0.0.1",
-    summary="A simple example package",
+    summary="A simple test package",
     author="plam",
     author_email="plam@student.42.fr",
     description="A simple example package",
