1464aecc0e7632a433c7c56888c3c6bd45b4d780
plam <plam@student.42.fr>
2025-01-25 23:59:51 +0100

    day01 started, ex0@ started

---
+++ Piscine_Python/Day01/ex02/load_image.py
@@ -0,0 +1,23 @@
+import numpy as np
+from PIL import Image
+
+
+def ft_load(path: str) -> np.ndarray:
+    try:
+        img = Image.open(path)
+        if img.format not in ['JPEG', 'JPG']:
+            raise ValueError(
+                "Unsupported image format. "
+                "Only JPG and JPEG are supported.")
+
+        img = img.convert('RGB')
+        img_array = np.array(img)
+
+        print(f"The shape of image is: {img_array.shape}")
+        print("Pixel content in RGB format:")
+        print(img_array)
+
+        return img_array
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
