35bdb6c877b1726d801ea3db6af0aa24347523ae
plam <plam@student.42.fr>
2025-01-27 17:53:55 +0100

    day01 started, ex03 started, tests done

---
+++ Piscine_Python/Day01/ex03/zoom.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+from load_image import ft_load
+
+
+def main():
+    """Main function to crop/zoom on an image."""
+    try:
+        # Load the image
+        image_path = "animal.jpeg"
+        image = ft_load(image_path)
+        image_array = np.array(image)
+
+        # Print image information
+        print(f"The shape of image is: {image_array.shape}")
+        print(image_array[:1])  # Print the first row of pixels
+
+        # Crop the image
+        cropped_image_array = image_array[:400, :400, :1]
+        print(f"New shape after slicing: {cropped_image_array.shape}")
+        print(cropped_image_array[:1])  # Print the first row of cropped pixels
+
+        # Display the cropped image with scales on x and y axes
+        plt.imshow(cropped_image_array.squeeze(), cmap='gray')
+        plt.title('Cropped Image')
+        plt.show()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
