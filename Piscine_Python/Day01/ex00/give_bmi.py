7992596ebca904f69b1675993070bfc807af3801
plam <plam@student.42.fr>
2025-01-30 16:16:11 +0100

    day01 ex00 error corrected

--- Piscine_Python/Day01/ex00/give_bmi.py
+++ Piscine_Python/Day01/ex00/give_bmi.py
@@ -1,20 +1,28 @@
 import numpy as np
 
 
-def give_bmi(
-    height: np.ndarray,
-    weight: np.ndarray
-) -> np.ndarray:
-    """returns a numpy array of BMI values calculated
-    from the given height and weight arrays"""
-    if height.shape != weight.shape:
-        raise ValueError("Height and weight arrays must be of the same shape.")
-    if not (np.issubdtype(height.dtype, np.number) and np.issubdtype(weight.dtype, np.number)):
+def give_bmi(height: list[int | float],
+             weight: list[int | float]
+             ) -> list[int | float]:
+    if len(height) != len(weight):
+        raise ValueError("Height and weight lists must be of the same length.")
+
+    height_array = np.array(height)
+    weight_array = np.array(weight)
+
+    if not (np.issubdtype(height_array.dtype, np.number)
+            and np.issubdtype(weight_array.dtype, np.number)):
         raise TypeError("Height and weight must be int or float.")
-    return weight / (height ** 2)
+
+    bmi_array = weight_array / (height_array ** 2)
+    return bmi_array.tolist()
 
 
-def apply_limit(bmi: np.ndarray, limit: int) -> np.ndarray:
-    if not np.issubdtype(bmi.dtype, np.number):
-        raise TypeError("BMI values must be numeric.")
-    return bmi > limit
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    bmi_array = np.array(bmi)
+
+    if not np.issubdtype(bmi_array.dtype, np.number):
+        raise TypeError("BMI values must be int or float.")
+
+    limit_array = bmi_array > limit
+    return limit_array.tolist()
