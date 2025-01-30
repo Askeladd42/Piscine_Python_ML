a94834931f5429371b541404f77c5d7fede04bed
plam <plam@student.42.fr>
2025-01-30 16:25:35 +0100

    day01 ex01 error corrected

--- Piscine_Python/Day01/ex01/array2D.py
+++ Piscine_Python/Day01/ex01/array2D.py
@@ -1,6 +1,8 @@
-import numpy
+import numpy as np
 
 
 def slice_me(family: list, start: int, end: int) -> list:
     """returns a slice of the given list from start to end"""
-    return numpy.array(family)[start:end].tolist()
+    print(f"My shape is : {np.array(family).shape}")
+    print(f"My new shape is : {np.array(family)[start:end].shape}")
+    return np.array(family)[start:end].tolist()
