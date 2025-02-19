b7a5482962d9c1377269748a59a065b4f13e0d78
Pierre-marie LAM <plam@bess-f2r3s12.clusters.42paris.fr>
2025-02-19 13:26:03 +0100

    day01 ex0 updated with error handlers

--- Piscine_Python/Day01/ex01/array2D.py
+++ Piscine_Python/Day01/ex01/array2D.py
@@ -3,6 +3,15 @@ import numpy as np
 
 def slice_me(family: list, start: int, end: int) -> list:
     """returns a slice of the given list from start to end"""
-    print(f"My shape is : {np.array(family).shape}")
-    print(f"My new shape is : {np.array(family)[start:end].shape}")
-    return np.array(family)[start:end].tolist()
+    try:
+        if not isinstance(family, list):
+            raise TypeError("Please provide a list")
+        if not isinstance(start, int) or not isinstance(end, int):
+            raise TypeError("Please provide integers for start and/or end")
+        if start < 0 or end > len(family):
+            raise ValueError("Invalid start or end indices")
+        print(f"My shape is : {np.array(family).shape}")
+        print(f"My new shape is : {np.array(family)[start:end].shape}")
+        return np.array(family)[start:end].tolist()
+    except Exception as e:
+        return e
