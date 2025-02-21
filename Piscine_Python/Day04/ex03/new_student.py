355917e454393ade8512588c82a94844f1c9ed8d
plam <plam@student.42.fr>
2025-02-21 23:55:27 +0100

    updated version of ex03 day04 using field function correctly

--- Piscine_Python/Day04/ex03/new_student.py
+++ Piscine_Python/Day04/ex03/new_student.py
@@ -4,22 +4,23 @@ from dataclasses import dataclass, field
 
 
 def generate_id() -> str:
-    return "".join(random.choices(string.ascii_lowercase, k = 15))
+    return "".join(random.choices(string.ascii_lowercase, k=15))
 
 
 @dataclass
 class Student:
     name: str = ""
     surname: str = ""
+    active: bool = field(default=True, init=False)
+    login: str = field(init=False)
+    id: str = field(default_factory=generate_id, init=False)
 
     def __post_init__(self):
-        self.active = True
         self.login = self.name.lower() + "." + self.surname.lower()
-        self.id = generate_id()
 
     def __str__(self):
         return f"Student {self.name} {self.surname} with ID {self.id}" + \
-            f" and login {self.login}"
+               f" and login {self.login}"
 
     def __repr__(self):
         return f"Student({self.name}, {self.surname})"
